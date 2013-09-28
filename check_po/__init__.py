d149c866d3fd15c7904a5b52f8455e7d83b75982
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2013-09-28 14:43:19 +0200

    Add Python3 compatibility

--- check_po/__init__.py
+++ check_po/__init__.py
@@ -1,12 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
 import sys
 import os.path
 import polib
 
 
 
+def writeout(*args):
+    """Wrapper around 'print' for Py2/3 compatibility."""
+    if sys.version_info[0] == 2:
+        # Need to send 'bytes' to print on Py2
+        args = [arg.encode('utf-8') for arg in args]
+    print(*args)
+
+
 def check_pofile(po_file_path):
     if not os.path.exists(po_file_path):
-        print "File not found %s\n" % po_file_path
+        writeout("File not found %s\n" % po_file_path)
         sys.exit(2)
 
     po = polib.pofile(po_file_path)
@@ -14,30 +26,30 @@ def check_pofile(po_file_path):
     UNTRANSLATED = 0
     for entry in po.untranslated_entries():
         UNTRANSLATED += 1
-        print "UNTRANSLATED\t", entry.msgid.encode('utf-8')
+        writeout("UNTRANSLATED\t", entry.msgid)
 
     FUZZY = 0
     for entry in po.fuzzy_entries():
         FUZZY += 1
-        print "FUZZY\t", entry.msgid.encode('utf-8'), entry.msgstr.encode('utf-8')
+        writeout("FUZZY\t", entry.msgid, entry.msgstr)
 
     if FUZZY or UNTRANSLATED:
-        print '\n----------'
+        writeout("\n----------")
         if FUZZY:
-            print "%d FUZZY string found." % FUZZY
+            writeout("%d FUZZY string found." % FUZZY)
         if UNTRANSLATED:
-            print "%d UNTRANSLATED string found." % UNTRANSLATED
-        print '----------'
-        print "Edit: %s\n\n" % os.path.abspath(po_file_path)
+            writeout("%d UNTRANSLATED string found." % UNTRANSLATED)
+        writeout("----------")
+        writeout("Edit: %s\n\n" % os.path.abspath(po_file_path))
         sys.exit(100)
 
-    print "%s: PO File OK\n\n" % po_file_path
+    writeout("%s: PO File OK\n\n" % po_file_path)
     sys.exit(0)
 
 
 def main():
     if len(sys.argv) != 2:
-        print "USAGE: %s <po_file_to_check>\n" % sys.argv[0]
+        writeout("USAGE: %s <po_file_to_check>\n" % sys.argv[0])
         sys.exit(1)
 
     po_file_path = sys.argv[1]
