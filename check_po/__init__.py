7def1f706b3f6b5619c6ad18d867a421171c2838
Raphaël Barrois <raphael.barrois@polyconseil.fr>
2013-09-28 14:55:14 +0200

    Add Python3 compatibility to other utils

--- check_po/__init__.py
+++ check_po/__init__.py
@@ -1,19 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
-
 import sys
 import os.path
 import polib
 
 
-
-def writeout(*args):
-    """Wrapper around 'print' for Py2/3 compatibility."""
-    if sys.version_info[0] == 2:
-        # Need to send 'bytes' to print on Py2
-        args = [arg.encode('utf-8') for arg in args]
-    print(*args)
+from .utils import writeout
 
 
 def check_pofile(po_file_path):
---
+++ check_po/utils.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+
+import sys
+
+
+def writeout(*args):
+    """Wrapper around 'print' for Py2/3 compatibility."""
+    if sys.version_info[0] == 2:
+        # Need to send 'bytes' to print on Py2
+        args = [arg.encode('utf-8') for arg in args]
+    print(*args)
