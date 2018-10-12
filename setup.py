669c21b491c9f14cef74b415fcbffeae72e75a82
Rémy HUBSCHER <remy@chefclub.tv>
2018-10-12 15:48:11 +0200

    Other cleanups.

--- setup.py
+++ setup.py
@@ -2,8 +2,6 @@
 from setuptools import setup
 
 setup(
-    # test_suite='nose.collector',
     setup_requires=['setuptools>=28.0', 'pbr>=1.9'],
-    # tests_require=['nose', 'coverage'],
     pbr=True,
 )
